e48390aeff70846c8feec62d5e5276742825a0fd
MayaMazlom <mayamazloum16@gmail.com>
2023-11-21 19:40:33 +0200

    cross lines game

---
+++ script.js
@@ -0,0 +1,116 @@
+const game = document.querySelector(".game-container")
+const char = document.querySelector(".character")
+
+let interval;
+let keyDown = false;
+
+// ball movement
+const moveLeft = () => {
+    let leftpos = parseInt(window.getComputedStyle(char).getPropertyValue("left"));
+    if (leftpos > 0)
+    {
+        char.style.left = leftpos - 2 + "px";
+    }
+} 
+
+const moveRight = () => {
+    let leftpos = parseInt(window.getComputedStyle(char).getPropertyValue("left"));
+    if (leftpos < 370)
+    {
+        char.style.left = leftpos + 2 + "px";
+    }
+} 
+
+// when click on button
+document.addEventListener("keydown" , (event) => {
+    if (!keyDown)
+    {
+        if(event.key == "ArrowLeft")
+        {
+            interval = setInterval(moveLeft,1)
+        }
+        else if(event.key == "ArrowRight")
+        {
+            interval = setInterval(moveRight,1)
+        }
+
+        keyDown = true;
+    }
+})
+
+// when not click on button
+document.addEventListener("keyup", () =>{
+    clearInterval(interval);
+    keyDown = false;
+})
+
+// generating obstacles
+const generateObstacles = () =>{
+    let block = document.createElement("div");
+    let hole = document.createElement("div");
+
+    block.setAttribute("class","block");
+    hole.setAttribute("class","hole");
+
+    let randHolePos = Math.floor(Math.random() * 320);
+    hole.style.left = randHolePos + "px";
+
+    game.appendChild(block)
+    game.appendChild(hole)
+
+    // to remove these elements at the end of animation
+    $('.block').bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd',function(e){$(this).remove();});
+    $('.hole').bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd',function(e){$(this).remove();});
+}
+
+setInterval(generateObstacles,1500)
+
+const checkCollisions = () => {
+    const allBlocks = document.querySelectorAll(".block")
+    const allHoles = document.querySelectorAll(".hole")
+
+    allBlocks.forEach(b => {
+        let hitObstacle = false;
+
+        if(collision(b , char))
+        {
+            hitObstacle = true
+
+            allHoles.forEach(h => {
+                if(holeCollision(h,char)){
+                    hitObstacle = false
+                }
+            })
+        }
+
+        if(hitObstacle){
+            alert("Game over!!")
+            location.reload()
+        }
+    })
+}
+
+setInterval(checkCollisions,1)
+
+// check obstacles collisions
+function collision(a,b){
+    let a_top = parseInt(window.getComputedStyle(a).getPropertyValue("top"));
+    let b_top = parseInt(window.getComputedStyle(b).getPropertyValue("top"));
+
+    return (
+        a_top + 20 > b_top && a_top < b_top + 20
+    );
+}
+
+// check hole collisions
+function holeCollision(h,b){
+    let h_left = parseInt(window.getComputedStyle(h).getPropertyValue("left"));
+    let h_top = parseInt(window.getComputedStyle(h).getPropertyValue("top"));
+
+    let b_left = parseInt(window.getComputedStyle(b).getPropertyValue("left"));
+    let b_top = parseInt(window.getComputedStyle(b).getPropertyValue("top"));
+
+    return(
+        b_left > h_left && b_left < h_left + 50 && h_top + 20 > b_top && h_top < b_top + 20
+    );
+}
